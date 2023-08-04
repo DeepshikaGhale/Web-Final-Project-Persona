29931bb8fd5ecacd10ade62e91ef977aaa967ac1
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-08-04 14:27:30 -0400

    Create journal project

---
+++ Persona/Models/JournalModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Persona.Models;
+
+public class JournalModel
+{
+	public int JournalID { get; set; }
+
+	public String Description { get; set; } = "";
+
+	public String photo { get; set; } = "";
+
+	public DateTime? UserEnteredDate { get; set; }
+
+	public DateTime? CreatedDate { get; set; }
+}
+
